71e239d773eea1ed7e03ad86cdccdb5680cf8efc
Fenix-LAB <fenixcr.123@gmail.com>
2024-02-28 21:10:08 -0600

    Update function to handle edge cases

---
+++ app/src/services/exam_types.py
@@ -0,0 +1,108 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from sqlalchemy import text
+from src.schemas.exam_types import ExamTypesRequest, ExamTypesUpdateRequest
+from src.utils.ctes import EXAM_TYPES_ROW
+from src.utils.helper import rows_to_dicts
+from datetime import datetime
+
+
+def get(db_session: Session):
+    """Get All Exam Types"""
+    try:
+        query = text("SELECT * FROM exam_types")
+        exam_types = db_session.execute(query).fetchall()
+
+        # Convert the list of tuples to a list of dictionaries
+        exam_types = rows_to_dicts(exam_types, EXAM_TYPES_ROW)
+        
+        return exam_types
+
+    except Exception as ex:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(ex),
+        ) from ex
+    
+
+
+def create(exam_type: ExamTypesRequest, db_session: Session):
+    """Create Exam Type"""
+    try:
+        # Generate the current datetime
+        created_at = datetime.now()
+        updated_at = datetime.now()
+
+        data_exam_type = {
+            "company_id": exam_type.company_id,
+            "exam_name": exam_type.exam_name,
+            "description": exam_type.description,
+            "status": exam_type.status,
+            "created_at": created_at,
+            "created_by": exam_type.created_by,
+            "updated_at": updated_at,
+            "updated_by": exam_type.updated_by
+        }
+
+        query = text("INSERT INTO exam_types (company_id, exam_name, description, status, created_at, created_by, updated_at, updated_by) VALUES (:company_id, :exam_name, :description, :status, :created_at, :created_by, :updated_at, :updated_by)")
+
+        db_session.execute(query, data_exam_type)
+
+        db_session.commit()
+
+        return {"message": "Exam Type created successfully"}
+
+    except Exception as ex:
+        db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(ex),
+        ) from ex
+    
+
+def update(exam_type: ExamTypesUpdateRequest, exam_type_id: int, db_session: Session):
+    """Update Exam Type"""
+    try:
+        # Generate the current datetime
+        updated_at = datetime.now()
+
+        data_exam_type = {
+            "company_id": exam_type.company_id,
+            "exam_name": exam_type.exam_name,
+            "description": exam_type.description,
+            "status": exam_type.status,
+            "updated_at": updated_at,
+            "updated_by": exam_type.updated_by
+        }
+
+        query = text("UPDATE exam_types SET company_id = :company_id, exam_name = :exam_name, description = :description, status = :status, updated_at = :updated_at, updated_by = :updated_by WHERE exam_type_id = :exam_type_id")
+
+        db_session.execute(query, {**data_exam_type, "exam_type_id": exam_type_id})
+
+        db_session.commit()
+
+        return {"message": "Exam Type updated successfully"}
+
+    except Exception as ex:
+        db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(ex),
+        ) from ex
+    
+
+def delete(exam_type_id: int, db_session: Session):
+    """Delete Exam Type"""
+    try:
+        query = text("DELETE FROM exam_types WHERE exam_type_id = :exam_type_id")
+        db_session.execute(query, {"exam_type_id": exam_type_id})
+        db_session.commit()
+
+        return {"message": "Exam Type deleted successfully"}
+
+    except Exception as ex:
+        db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(ex),
+        ) from ex
