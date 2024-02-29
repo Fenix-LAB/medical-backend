7520538eacfac26f7a4a3f6fc86a1393a7fb1588
Fenix-LAB <fenixcr.123@gmail.com>
2024-02-28 21:23:45 -0600

    Add exams schema

--- app/src/utils/ctes.py
+++ app/src/utils/ctes.py
@@ -4,3 +4,4 @@ COMPANIES_ROW = ['company_id', 'commercial_name', 'contact_person_id', 'status',
 PERSONS_ROW = ['person_id', 'first_name', 'last_name', 'identification_type', 'identification', 'birthdate', 'gender', 'marital_status', 'address', 'phone_number', 'email', 'created_at', 'created_by', 'updated_at', 'updated_by', 'company_id']
 DISEASE_TYPES_ROW = ['disease_type_id', 'disease_name', 'description', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by']
 EXAM_TYPES_ROW = ['exam_type_id', 'company_id', 'exam_name', 'description', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by']
+EXAMS_ROW = ['exam_id', 'exam_type_id', 'company_id', 'exam_name', 'description', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by']
