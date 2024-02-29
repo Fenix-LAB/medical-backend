09455bdecd2003c58ff4b0b28fb867646337a205
Fenix-LAB <fenixcr.123@gmail.com>
2024-02-28 20:36:32 -0600

    Add DISEASE_TYPES_ROW constant to ctes.py

--- app/src/utils/ctes.py
+++ app/src/utils/ctes.py
@@ -2,3 +2,4 @@
 
 COMPANIES_ROW = ['company_id', 'commercial_name', 'contact_person_id', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by']
 PERSONS_ROW = ['person_id', 'first_name', 'last_name', 'identification_type', 'identification', 'birthdate', 'gender', 'marital_status', 'address', 'phone_number', 'email', 'created_at', 'created_by', 'updated_at', 'updated_by', 'company_id']
+DISEASE_TYPES_ROW = ['disease_type_id', 'disease_name', 'description', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by']
