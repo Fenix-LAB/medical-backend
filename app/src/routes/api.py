2b62412192b38efb2073330e45cafb01bde5b392
Fenix-LAB <fenixcr.123@gmail.com>
2024-02-28 21:05:03 -0600

    Add routes for persons, disease types, and exam types

--- app/src/routes/api.py
+++ app/src/routes/api.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, status
 
 from .router import (
     companies,
+    persons,
+    disease_types,
+    exam_types
 )
 
 router = APIRouter()
@@ -12,3 +15,5 @@ async def server_start():
     return {"message": "Welcome medical, server is up and running"}
 
 router.include_router(companies.router, tags=["companies"])
+router.include_router(persons.router, tags=["persons"])
+router.include_router(disease_types.router, tags=["disease_types"])
